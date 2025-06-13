66de50766858ebfba9abfe021529154151a8cc52
Alex Tellez <alextellezyanes@gmail.com>
2025-06-13 10:17:35 +0200

    add employee repository and create employee service

---
+++ backend/src/main/java/com/AlteyaHR/service/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.AlteyaHR.service;
+
+import com.AlteyaHR.model.Employee;
+import com.AlteyaHR.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee){
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+}
