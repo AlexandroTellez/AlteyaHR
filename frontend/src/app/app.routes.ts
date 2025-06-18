17b975cb6a5771ecdb48ac0c8efafcb97d998695
Alex Tellez <alextellezyanes@gmail.com>
2025-06-18 16:54:22 +0200

    add new project structure and page employee with navbar and table

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { EmployeePageComponent } from './features/employee/pages/employee-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'employees', pathMatch: 'full' },
+    { path: 'employees', component: EmployeePageComponent },
+];
